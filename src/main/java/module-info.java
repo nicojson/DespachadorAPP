9b55935f2b58cedbdcea76ec40da7a948afa5563
libertyCode <23031391@itcelaya.edu.mx>
2025-10-14 22:29:16 -0600

    ejecutable listo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,17 @@
 module tecnm.celaya.edu.mx.despachadorapp {
+    // Módulos de JavaFX requeridos por la aplicación
     requires javafx.controls;
     requires javafx.fxml;
 
+    // Módulos de dependencias externas
     requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
+    //requires formsfx.core; // Nombre de módulo corregido
     requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
+    //requires bootstrapfx.core; // Nombre de módulo corregido
 
+    // Permite que JavaFX acceda a los controladores y al modelo de datos mediante reflexión.
     opens tecnm.celaya.edu.mx.despachadorapp to javafx.fxml;
+
+    // Exporta el paquete principal para que la aplicación pueda ser lanzada.
     exports tecnm.celaya.edu.mx.despachadorapp;
 }
